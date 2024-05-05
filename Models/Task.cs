158766f376d83d84e163e7eb5824c5a1e1efb3aa
Adam <Adam@THE-POTATO>
2024-05-05 20:58:15 +0100

    Add project files.

---
+++ Models/Task.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string? Name { get; set; }
+
+    [StringLength (60, MinimumLength = 3)]
+    public string? Description { get; set; } = null;
+
+    [Display(Name = "Created Date")]
+    [DataType(DataType.Date)]
+    public DateTime? CreatedDate { get; set; } = DateTime.Today;
+
+    [Display(Name = "Due Date")]
+    [DataType(DataType.Date)]
+    public DateTime? DueDate { get; set; }
+
+    [Range(1, 3)]
+    public int Priority { get; set; }
+
+    [StringLength(60, MinimumLength =3)]
+    [Required]
+    public string? Category { get; set; }
+}
---
+++ Models/TaskCategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ToDoList.Models;
+
+public class TaskCategoryViewModel
+{
+    public List<Task>? Tasks { get; set; }
+    public SelectList? Categories { get; set; }
+    public string? TaskCategory {  get; set; }
+    public string? SearchString { get; set; }
+}
